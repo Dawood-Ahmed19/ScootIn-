c78b8d6618c0d1974a747029a0dfbe000e7c34c3
Dawood-Ahmed19 <gfxwork66@gmail.com>
2023-07-25 20:34:52 +0500

    Feat: added cards data

---
+++ src/components/CardsData.jsx
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "Locate with App",
+    details:
+      "Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away. ",
+    image: "../assets/Images/iconOne.png",
+  },
+  {
+    id: 2,
+    title: "Pick Your Scooter",
+    details:
+      "We show the most important info for the scooters closest to you. So you know how much charge they have left and can see roughly how much it will cost. ",
+    image: "../assets/Images/iconTwo.png",
+  },
+  {
+    id: 3,
+    title: "Enjoy Your Ride",
+    details:
+      "Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet, and you’re off! Always lock bikes away from walkways and accessibility ramps. ",
+    image: "../assets/Images/iconThree.png",
+  },
+];
---
+++ src/components/card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const card = (props) => {
+  return (
+    <>
+      <div className="card--container">
+        <div className="card--image">
+          <img src={props.image} alt="card-image" />
+        </div>
+        <div className="card--title">
+          <h4>{props.title}</h4>
+        </div>
+        <div className="card--details">{props.details}</div>
+      </div>
+    </>
+  );
+};
+
+export default card;
