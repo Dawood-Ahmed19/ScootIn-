c18ce7066b7d5bb378aeabe80e55de7a6487dcde
Dawood-Ahmed19 <gfxwork66@gmail.com>
2023-07-26 16:30:36 +0500

    Feat: added About Page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const About = () => {
-  return;
+  return (
+    <>
+      {/* Header */}
+
+      <header className="abt--header">
+        <div className="container">
+          <div className="about--title">
+            <h1>About</h1>
+          </div>
+        </div>
+      </header>
+
+      {/*  */}
+    </>
+  );
 };
 
 export default About;
